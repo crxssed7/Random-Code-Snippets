e29e127b9e91edc390bb8e0868de562bf3f490f8
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2020-12-09 21:00:56 +0000

    Fixed Formatting

--- C#/Functions/CompressImage.cs
+++ C#/Functions/CompressImage.cs
@@ -9,16 +9,16 @@ namespace RWFCS
 		// eg, if CompressSize = 2 then the resulting image will be half the size as the original.
 		// Useful for resizing images to thumbnail/picturebox size and reducing memory used by project.
 		public Bitmap CompressImage(string ImageFilePath, int CompressSize)
-        {
-            using (Image img = Image.FromFile(ImageFilePath))
-            {
-                Bitmap bmp = new Bitmap(img.Width / CompressSize, img.Height / CompressSize);
-                using (Graphics g = Graphics.FromImage(bmp))
-                {
-                    g.DrawImage(img, new Rectangle(0, 0, bmp.Width, bmp.Height));
-                }
-                return bmp;
-            }
-        }
+		{
+			using (Image img = Image.FromFile(ImageFilePath))
+			{
+				Bitmap bmp = new Bitmap(img.Width / CompressSize, img.Height / CompressSize);
+				using (Graphics g = Graphics.FromImage(bmp))
+				{
+					g.DrawImage(img, new Rectangle(0, 0, bmp.Width, bmp.Height));
+				}
+				return bmp;
+			}
+		}
 	}
 }
