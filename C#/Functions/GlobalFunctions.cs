8a8fdce0e186cf0d9d1d3bed3fe4597df6cfdc62
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-01-24 11:23:10 +0000

    Update GlobalFunctions.cs

--- C#/Functions/GlobalFunctions.cs
+++ C#/Functions/GlobalFunctions.cs
@@ -56,6 +56,59 @@ namespace YourNameSpace
                 return newImage;
             }
         }
+		
+		// Algorithm utilises the SizeMode.CenterImage property on a PictureBox, so it accurately works out the proportion the 
+		// image must be in order to not be squished.
+        // It also utilises the ResizeImage function (see below).
+        public static Bitmap CropImageCenter(string imageLocation, PictureBox pictureBox1)
+        {
+            using (Image image = Image.FromFile(imageLocation))
+            {
+                // If the Width of the image is bigger than the height, then we know the sides will be cut off
+                if (image.Width > image.Height)
+                {
+                    // The algorithm works by making the height of the final image the same as the height of the PictureBox (this is different for width, see else statement), 
+                    // but we first need to find out what the final width will be so the image doesn't come out as squished.
+                    // This formula determines the number we need to multiply by to get an unstretched image
+                    double multiplier = Math.Round((double)(pictureBox1.Height) / image.Height, 2);
+
+                    int newHeight = pictureBox1.Height;
+                    // Apply that multiplier to the width to get the final width of the image
+                    double newWidth = image.Width * multiplier;
+
+                    return ResizeImage(image, Convert.ToInt32(Math.Round(newWidth, 0)), newHeight);
+                }
+                else
+                {
+                    // Same as above, however the top and bottom will be cut instead, so we do the calucaltion around width instead.
+                    double multiplier = Math.Round((double)(pictureBox1.Width) / image.Width, 2);
+
+                    double newHeight = image.Height * multiplier;
+                    int newWidth = pictureBox1.Width;
+
+                    return ResizeImage(image, newWidth, Convert.ToInt32(Math.Round(newHeight, 0)));
+                }
+            }
+        }
+
+        // Resizes an image with the given height and width.
+        // Courtesy of Nordic16 (https://github.com/nordic16)
+        public static Bitmap ResizeImage(Image img, int width, int height)
+        {
+            var destImage = new Bitmap(width, height);
+
+            using (Graphics graphics = Graphics.FromImage(destImage))
+            {
+                using (var wMode = new ImageAttributes())
+                {
+                    var rect = new Rectangle(0, 0, width, height);
+                    wMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(img, rect, 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, wMode);
+                }
+            }
+
+            return destImage;
+        }
 
 		#endregion
 	}
