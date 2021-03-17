af7a761e89aa20a74433e88809df169591f82b69
Tanveer Najib <48864420+crxssed7@users.noreply.github.com>
2021-03-17 15:25:01 +0000

    feat: add get request

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,2 +1,15 @@
 // Generate a random number between 1 - 100
 let rand = Math.floor((Math.random() * 100) + 1);
+
+// A get request using JQuery. Get the data from JsonPlaceHolder, then update the UI accordingly
+$.get(
+    "https://jsonplaceholder.typicode.com/todos/1",
+    function(data) {
+       console.log(data);
+
+       document.getElementById("title").innerHTML += data.title;
+       document.getElementById("objId").innerHTML += data.id;
+       document.getElementById("objCustId").innerHTML += data.userId;
+       document.getElementById("completed").checked = data.completed;
+    }
+);
